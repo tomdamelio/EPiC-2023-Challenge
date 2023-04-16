3f1bb3cae26cfbf930b5f170a7fa068acc87ec8c
tomdamelio <tomasdamelio@psi.uba.ar>
2023-04-16 17:46:22 -0300

    add config - physiology freqs

---
+++ src/config.py
@@ -0,0 +1,56 @@
+physiology_freqs = {
+    "gsr": {
+        "gsr_freq_1": [0.0033, 0.04],
+        "gsr_freq_2": [0.04, 0.15],
+        "gsr_freq_3": [0.15, 0.40],
+        "gsr_freq_4": [0.4, 0.5],
+    },
+    "BVP": {
+        "BVP_freq_1": [0.5, 0.8],
+        "BVP_freq_2": [0.8, 1.3],
+        "BVP_freq_3": [1.3, 1.8],
+        "BVP_freq_4": [1.8, 2.5],
+    },
+    "ecg": {
+        "ecg_freq_1": [0.045, 0.15],
+        "ecg_freq_2": [0.15, 0.25],
+        "ecg_freq_3": [0.25, 0.4],
+        "ecg_freq_4": [0.4, 0.5],
+        "ecg_freq_5": [0.5, 1.0],
+        "ecg_freq_6": [1.0, 2.0],
+        "ecg_freq_7": [2.0, 3.0],
+        "ecg_freq_8": [3.0, 4.0],
+        "ecg_freq_9": [4.0, 5.0],
+    },
+    "emg_coru": {
+        "emg_coru_freq_1": [30.0, 100.0],
+        "emg_coru_freq_2": [100.0, 200.0],
+        "emg_coru_freq_3": [200.0, 300.0],
+        "emg_coru_freq_4": [300.0, 400.0],
+        "emg_coru_freq_5": [400.0, 500.0],
+    },
+    "emg_zygo": {
+        "emg_zygo_freq_1": [30.0, 100.0],
+        "emg_zygo_freq_2": [100.0, 200.0],
+        "emg_zygo_freq_3": [200.0, 300.0],
+        "emg_zygo_freq_4": [300.0, 400.0],
+        "emg_zygo_freq_5": [400.0, 500.0],
+    },
+    "emg_trap": {
+        "emg_trap_freq_1": [30.0, 100.0],
+        "emg_trap_freq_2": [100.0, 200.0],
+        "emg_trap_freq_3": [200.0, 300.0],
+        "emg_trap_freq_4": [300.0, 400.0],
+        "emg_trap_freq_5": [400.0, 500.0],
+    },
+    "rsp": {
+        "rsp_freq_1": [0.00, 0.04],
+        "rsp_freq_2": [0.04, 0.15],
+        "rsp_freq_3": [0.15, 0.40],
+    },
+    "skt": {
+        "skt_freq_1": [0.01, 0.1],
+        "skt_freq_2": [0.1, 0.6],
+        "skt_freq_3": [0.6, 1.0],
+    },
+}
